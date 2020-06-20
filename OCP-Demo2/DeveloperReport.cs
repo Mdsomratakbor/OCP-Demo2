fac7fd44b3b4369b1258b37ba080c5777da00327
Mdsomratakbor <somrat876@gmail.com>
2020-06-20 20:01:04 +0600

    DeveloperReport class and IDeveloper Report interface add

---
+++ OCP-Demo2/DeveloperReport.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCP_Demo2
+{
+    public class DeveloperReport : IDeveloperReport
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Level { get; set; }
+        public string WorkingHours { get; set; }
+        public double HourlyRate { get; set; }
+
+    }
+}
---
+++ OCP-Demo2/IDeveloperReport.cs
@@ -0,0 +1,11 @@
+﻿namespace OCP_Demo2
+{
+    public interface IDeveloperReport
+    {
+        double HourlyRate { get; set; }
+        int ID { get; set; }
+        string Level { get; set; }
+        string Name { get; set; }
+        string WorkingHours { get; set; }
+    }
+}
