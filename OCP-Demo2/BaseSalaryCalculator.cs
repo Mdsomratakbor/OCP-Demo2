9b55dd537bd93194f8d406b15ff11317f377f737
Mdsomratakbor <somrat876@gmail.com>
2020-06-20 20:10:22 +0600

    BaseSalaryCalculator class add

---
+++ OCP-Demo2/BaseSalaryCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCP_Demo2
+{
+    public abstract class BaseSalaryCalculator
+    {
+        protected DeveloperReport DeveloperReport { get; set; }
+        public BaseSalaryCalculator(DeveloperReport developerReport)
+        {
+            DeveloperReport = developerReport;
+        }
+        public abstract double CalculateSalary();
+    }
+}
