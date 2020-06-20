950453930d4dbef94acedc214ca538d8f448c975
Mdsomratakbor <somrat876@gmail.com>
2020-06-20 20:18:22 +0600

    JuniorDevSalaryCalculator class add

---
+++ OCP-Demo2/JuniorDevSalaryCalculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCP_Demo2
+{
+    public class JuniorDevSalaryCalculator : BaseSalaryCalculator
+    {
+        public JuniorDevSalaryCalculator(DeveloperReport report):base(report)
+        {
+
+        }
+        public override double CalculateSalary() => DeveloperReport.HourlyRate * DeveloperReport.WorkingHours;
+    }
+}
